0c3b112c1124ac6c761a4ffcd9603482a93f52fe
Justyna Gruba <justyna.gruba@deployed.pl>
2025-05-18 17:55:42 +0200

    Project setup

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+
+const config = {
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+};
+
+export default config;
