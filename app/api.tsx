809c6203d14b2264135ae33580535528fc2b37db
Justyna Gruba <justyna.gruba@deployed.pl>
2025-05-19 23:49:13 +0200

    Add animaton to quote.

--- app/api.tsx
+++ app/api.tsx
@@ -4,8 +4,11 @@ import { useQuery } from '@tanstack/react-query';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ActivityIndicator, Button, Card, Switch } from 'react-native-paper';
 import { useEffect, useState } from 'react';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const Api = () => {
+	const opacity = useSharedValue(0);
+	const translateY = useSharedValue(20);
 	const [autoRefresh, setAutoRefresh] = useState(false);
 	const {
 		data: quote,
@@ -16,6 +19,19 @@ const Api = () => {
 		queryKey: ['quote'],
 		queryFn: fetchQuote,
 	});
+
+	useEffect(() => {
+		opacity.value = 0;
+		translateY.value = 20;
+
+		opacity.value = withTiming(1, { duration: 500 });
+		translateY.value = withTiming(0, { duration: 500 });
+	}, [quote]);
+
+	const animatedStyle = useAnimatedStyle(() => ({
+		opacity: opacity.value,
+		transform: [{ translateY: translateY.value }],
+	}));
 
 	useEffect(() => {
 		let interval: number | null = null;
@@ -52,14 +68,16 @@ const Api = () => {
 	return (
 		<SafeAreaView style={{ flex: 1 }} edges={['left', 'right', 'bottom']}>
 			<View style={styles.container}>
-				<Card mode="contained" style={styles.card}>
-					<Card.Content>
-						<Text style={styles.quote}>{quote?.quote}</Text>
-						<Text style={styles.author}>{quote?.author}</Text>
-					</Card.Content>
-				</Card>
+				<Animated.View style={animatedStyle}>
+					<Card mode="contained" style={styles.card}>
+						<Card.Content>
+							<Text style={styles.quote}>{quote?.quote}</Text>
+							<Text style={styles.author}>{quote?.author}</Text>
+						</Card.Content>
+					</Card>
+				</Animated.View>
 				<View style={styles.button}>
-					<View style={styles.autoRefechButton}>
+					<View style={styles.autoRefreshButton}>
 						<Text>Auto losowanie</Text>
 						<Switch value={autoRefresh} onValueChange={() => setAutoRefresh(!autoRefresh)} />
 					</View>
@@ -96,7 +114,7 @@ const styles = StyleSheet.create({
 		marginHorizontal: 50,
 		gap: 30,
 	},
-	autoRefechButton: {
+	autoRefreshButton: {
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'center',
