3259318cbbf075caf2d148ea6617716733356dee
Justyna Gruba <justyna.gruba@deployed.pl>
2025-05-19 19:20:04 +0200

    Add modal to valid form.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,26 @@
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
 	return (
 		<SafeAreaProvider>
-			<Stack
-				screenOptions={{
-					animation: 'fade',
-					headerStyle: {
-						backgroundColor: '#f4511e',
-					},
-					headerTitleAlign: 'center',
-					headerTintColor: '#fff',
-				}}
-			>
-				<Stack.Screen name="index" options={{ headerTitle: 'Zadanie rekrutacyjne' }} />
-				<Stack.Screen name="form" options={{ headerTitle: '' }} />
-				<Stack.Screen name="api" options={{ headerTitle: '' }} />
-			</Stack>
+			<PaperProvider>
+				<Stack
+					screenOptions={{
+						animation: 'fade',
+						headerStyle: {
+							backgroundColor: '#f4511e',
+						},
+						headerTitleAlign: 'center',
+						headerTintColor: '#fff',
+					}}
+				>
+					<Stack.Screen name="index" options={{ headerTitle: 'Zadanie rekrutacyjne' }} />
+					<Stack.Screen name="form" options={{ headerTitle: '' }} />
+					<Stack.Screen name="api" options={{ headerTitle: '' }} />
+				</Stack>
+			</PaperProvider>
 		</SafeAreaProvider>
 	);
 }
--- app/form.tsx
+++ app/form.tsx
@@ -1,10 +1,11 @@
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useForm } from 'react-hook-form';
 import { Input } from '@/components/Input';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from 'react-native-paper';
+import { Button, Modal, Text, PaperProvider, Portal } from 'react-native-paper';
 import { z } from 'zod';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
 export const schema = z.object({
 	name: z.string().min(5, 'Imię musi zawierać więcej niż 5 znaków!'),
@@ -17,21 +18,33 @@ export type FormValues = {
 };
 
 const Form = () => {
+	const [visible, setVisible] = useState(false);
 	const { control, handleSubmit } = useForm<FormValues>({
 		resolver: zodResolver(schema),
 	});
 
 	const onSubmit = (data: FormValues) => {
 		console.log('Dane formularza:', data);
+		setVisible(true);
 	};
 
+	const hideModal = () => setVisible(false);
+
 	return (
-		<SafeAreaView style={styles.container}>
-			<Input control={control} name="name" />
-			<Input control={control} name="email" />
-			<Button mode="contained" onPress={handleSubmit(onSubmit)}>
-				Wyślij
-			</Button>
+		<SafeAreaView style={{ flex: 1 }} edges={['left', 'right']}>
+			<View style={styles.container}>
+				<Input control={control} name="name" />
+				<Input control={control} name="email" />
+				<Button mode="contained" onPress={handleSubmit(onSubmit)}>
+					Wyślij
+				</Button>
+			</View>
+			<Portal>
+				<Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modal}>
+					<Text>Dziękujemy, dane zapisane!</Text>
+					<Button onPress={hideModal}>Zamknij</Button>
+				</Modal>
+			</Portal>
 		</SafeAreaView>
 	);
 };
@@ -43,5 +56,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		margin: 20,
 		gap: 20,
+	},
+	modal: {
+		backgroundColor: 'white',
+		padding: 20,
+		margin: 50,
+		borderRadius: 10,
 	},
 });
